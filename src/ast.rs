6cc67a229f8f34af8cf2ac171090176c33544845
kn-shimada <xianrendaotian012@gmail.com>
2021-06-02 07:38:26 +0000

    コメント

--- src/ast.rs
+++ src/ast.rs
@@ -2,11 +2,13 @@
 pub enum Expr {
     Number(isize),
 
+    //例: -10
     PrefixExpr {
         operator: String,
         right: Box<Expr>,
     },
 
+    //例: 1 + 2
     InfixExpr {
         left: Box<Expr>,
         operator: String,
