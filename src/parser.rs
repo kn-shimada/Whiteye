4fbf817207042bcbbd93f999365e00de66ba5546
kn-shimada <xianrendaotian012@gmail.com>
2021-06-08 08:17:22 +0000

    前置演算子式と数値の葉の解析の実装

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 use crate::ast::Expr;
-//lexerのuseの仕方が分からん
+use crate::lexer;
 
 
 pub struct Perser {
@@ -14,6 +14,33 @@ impl parser {
             lexer: Lexer,
             cur: lexer.lex(),
             peek: lexer.lex(),
+        }
+    }
+
+    // 構文木の葉の要素の解析
+    fn parse_prefix(&mut self) -> Option<Box<Expr>> {
+        match self.cur.as_ref()? {
+            Token::Minus => self.parse_minus(),
+            Token::Number(_) => self.parse_number(),
+            _ => None,
+        }
+    }
+
+    // -が来た時の前置演算子式の解析
+    fn parse_minus(&mut self) -> Option<Box<Expr>> {
+        self.next();
+        let number = self.parse_prefix()?;
+        return Some(Box::new(Expr::PrefixExpr {
+            operator: "Minus".to_string(),
+            right: number,
+        }));
+    }
+
+    // 数値が来た時の解析
+    fn parse_number(&mut self) -> Option<Box<Expr>> {
+        match self.cur.borrow() {
+            Some(Token::Number(n)) => Some(Box::new(Expr::Number(*n))),
+            _ => None,
         }
     }
 
