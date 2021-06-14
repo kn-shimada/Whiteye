2e3139f3b2d466bf915ab9d6cbf4498c54bf4670
kn-shimada <xianrendaotian012@gmail.com>
2021-06-15 02:02:03 +0900

    どどどん

--- src/ast.rs
+++ src/ast.rs
@@ -1,12 +1,20 @@
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, PartialEq)]
+pub enum OpKind {
+    Add,
+    Sub,
+    Mul,
+    Div
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Ast {
     Number(isize),
 
     //例: 1 + 2
     Expr {
         left: Box<Ast>,
-        operator: String,
-        right: Box<Expr>,
+        operator: OpKind,
+        right: Box<Ast>,
     },
 }
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 use crate::lexer::Lexer;
-use crate::token::Token;
+use crate::token::{Token, TokenKind};
+use crate::ast::Ast;
 use anyhow::Result;
 
 pub struct Parser<'a> {
@@ -19,9 +20,30 @@ impl<'a> Parser<'a> {
         })
     }
 
-    pub fn next_token(&mut self) -> Result<()> {
+    pub fn parse(&self) {
+        match self.cur.kind {
+            TokenKind::Number(n) => self.parse_number(n),
+            TokenKind::Plus => todo!(),
+            TokenKind::Minus => todo!(),
+            TokenKind::Asterisk => todo!(),
+            TokenKind::Slash => todo!(),
+            TokenKind::LParen => todo!(),
+            TokenKind::RParen => todo!(),
+        };
+    }
+
+    fn parse_number(&self, n: isize) -> Box<Ast>{
+        Box::new(Ast::Number(n));
+        next_token()
+    }
+
+    fn _next_token(&mut self) -> Result<()> {
         self.cur = self.peek.clone();
         self.peek = self.lexer.lex()?;
         Ok(())
     }
+}
+
+fn next_token() -> Box<Ast> {
+    todo!()
 }
