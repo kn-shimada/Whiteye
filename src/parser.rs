c398bbde7e33fb4b50205d6c7b35d9370017cc7f
kn-shimada <xianrendaotian012@gmail.com>
2021-07-16 10:58:28 +0900

    fix: fix parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,38 +1,33 @@
 use nom::character::complete::{digit1, one_of};
 use nom::error::VerboseError;
-use nom::branch::alt;
 use nom::multi::many0;
+use nom::sequence::tuple;
 use nom::IResult;
 
 use crate::ast::{Ast, OpKind};
 
-pub fn parse(input: &str) -> IResult<&str, Vec<Ast>, VerboseError<&str>> {
-    let (input, a) = many0(alt((parse_expr, parse_number)))(input)?;
-    Ok((input, a))
+pub fn parse(input: &str) -> IResult<&str, Ast, VerboseError<&str>> {
+    let (input, num1) = parse_number(input)?;
+    let (input, exprs) = many0(tuple((parse_operator, parse_number)))(input)?;
+    Ok((input, parse_expr(num1, exprs)))
 }
 
-fn parse_expr(input: &str) -> IResult<&str, Ast, VerboseError<&str>> {
-    let (input, l) = parse_number(input)?;
-    let (input, o) = parse_operator(input)?;
-    let (input, r) = alt((parse_expr, parse_number))(input)?;
-    Ok((input,
-        Ast::Expr{
-            left: Box::new(l),
-            operator: o,
-            right: Box::new(r),
-        }))
+fn parse_expr(expr: Ast, rem: Vec<(OpKind, Ast)>) -> Ast {
+    rem.into_iter().fold(expr, |acc, val| Ast::Expr {
+        left: Box::new(acc),
+        operator: val.0,
+        right: Box::new(val.1),
+    })
 }
 
 fn parse_operator(input: &str) -> IResult<&str, OpKind, VerboseError<&str>> {
-    let (input, op) = one_of("+-*/")(input)?;
+    let (input, op) = one_of("+-")(input)?;
     Ok((
         input,
         match op {
             '+' => OpKind::Add,
             '-' => OpKind::Sub,
-            '*' => OpKind::Mul,
-            '/' => OpKind::Div,
-            _ => unreachable!()
+            _ => unreachable!(),
         },
     ))
 }
