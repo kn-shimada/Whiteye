20b2504eec053b65deffe3ef70929db66cc00f7a
kn-shimada <xianrendaotian012@gmail.com>
2021-08-04 07:52:41 +0900

    updata(main): Change log message

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use whiteye::parser::parse;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("program.txt").expect("Failed to read the file");
-    println!("Expr: {:?}", &input);
+    println!("program: {:?}", &input);
     let (input, parsed) = parse(&input).unwrap();
     if !input.is_empty() {
         eprintln!("parsing error, input remaining {:?}", input);
