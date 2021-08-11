21505c9540ad082875d9168249869fb0bc50eaea
kn-shimada <xianrendaotian012@gmail.com>
2021-08-11 23:52:50 +0900

    fix(machine): fix architecture

--- src/machine.rs
+++ src/machine.rs
@@ -12,7 +12,6 @@ pub enum Variable {
 
 #[derive(Debug, Default)]
 pub struct Machine {
-    pub current: Option<isize>,
     pub variables: HashMap<String, Variable>,
 }
 
@@ -22,73 +21,17 @@ impl Machine {
     }
 
     pub fn run(&mut self, expr: Ast) {
-        self.current = self.eval(expr);
-    }
-
-    pub fn eval(&mut self, expr: Ast) -> Option<isize> {
         match expr {
-            Ast::Number(num) => Some(num),
-
-            Ast::Variable(name) => match self.variables.get(&name).unwrap() {
-                Variable::Int(n) => Some(*n),
-            },
-
-            Ast::Expr {
-                left,
-                operator: ExprOpKind::EAdd,
-                right,
-            } => Some(self.eval(*left).unwrap() + self.eval(*right).unwrap()),
-
-            Ast::Expr {
-                left,
-                operator: ExprOpKind::ESub,
-                right,
-            } => Some(self.eval(*left).unwrap() - self.eval(*right).unwrap()),
-
-            Ast::Expr {
-                left,
-                operator: ExprOpKind::EMul,
-                right,
-            } => Some(self.eval(*left).unwrap() * self.eval(*right).unwrap()),
-
-            Ast::Expr {
-                left,
-                operator: ExprOpKind::EDiv,
-                right,
-            } => Some(self.eval(*left).unwrap() / self.eval(*right).unwrap()),
-
-            Ast::Expr {
-                left,
-                operator: ExprOpKind::EExp,
-                right,
-            } => Some(
-                self.eval(*left)
-                    .unwrap()
-                    .pow(self.eval(*right).unwrap().try_into().unwrap()),
-            ),
-
-            Ast::Monomial {
-                operator: UnaryOpKind::UPlus,
-                expr,
-            } => Some(self.eval(*expr).unwrap()),
-
-            Ast::Monomial {
-                operator: UnaryOpKind::UMinus,
-                expr,
-            } => Some(-self.eval(*expr).unwrap()),
-
             Ast::VariableDeclaration {
                 name,
                 data_type,
                 expr,
             } => {
                 let variable = match data_type {
-                    VariableType::Int => Variable::Int(self.eval(*expr).unwrap()),
+                    VariableType::Int => Variable::Int(self.eval_math_expr(*expr)),
                 };
 
                 self.variables.insert(name, variable);
-
-                None
             }
 
             Ast::VariableAssignment {
@@ -96,8 +39,10 @@ impl Machine {
                 operator,
                 expr,
             } => {
-                let v_expr = self.eval(*expr).unwrap();
+                let v_expr = self.eval_math_expr(*expr);
                 let Variable::Int(v_value) = self.variables.get(&name).unwrap();
+                dbg!(&v_expr);
+                dbg!(v_value);
                 let next_variable = match operator {
                     AssignmentOpKind::AEqual => Variable::Int(v_expr),
                     AssignmentOpKind::AAdd => Variable::Int(v_value + v_expr),
@@ -111,18 +56,70 @@ impl Machine {
                         panic!("Undefined variable");
                     }
                 };
-
-                None
             }
 
             Ast::FunctionCall { name, argument } => {
                 match name.as_ref() {
-                    "print" => builtin_functions::print(self.eval(*argument).unwrap()),
+                    "print" => builtin_functions::print(self.eval_math_expr(*argument)),
                     _ => panic!("Unknown Function name"),
                 };
-
-                None
             }
+
+            _ => println!("{}", self.eval_math_expr(expr)),
+        };
+    }
+
+    pub fn eval_math_expr(&mut self, expr: Ast) -> isize {
+        match expr {
+            Ast::Number(num) => num,
+
+            Ast::Variable(name) => match self.variables.get(&name).unwrap() {
+                Variable::Int(n) => *n,
+            },
+
+            Ast::Expr {
+                left,
+                operator: ExprOpKind::EAdd,
+                right,
+            } => self.eval_math_expr(*left) + self.eval_math_expr(*right),
+
+            Ast::Expr {
+                left,
+                operator: ExprOpKind::ESub,
+                right,
+            } => self.eval_math_expr(*left) - self.eval_math_expr(*right),
+
+            Ast::Expr {
+                left,
+                operator: ExprOpKind::EMul,
+                right,
+            } => self.eval_math_expr(*left) * self.eval_math_expr(*right),
+
+            Ast::Expr {
+                left,
+                operator: ExprOpKind::EDiv,
+                right,
+            } => self.eval_math_expr(*left) / self.eval_math_expr(*right),
+
+            Ast::Expr {
+                left,
+                operator: ExprOpKind::EExp,
+                right,
+            } => self
+                .eval_math_expr(*left)
+                .pow(self.eval_math_expr(*right).try_into().unwrap()),
+
+            Ast::Monomial {
+                operator: UnaryOpKind::UPlus,
+                expr,
+            } => self.eval_math_expr(*expr),
+
+            Ast::Monomial {
+                operator: UnaryOpKind::UMinus,
+                expr,
+            } => -self.eval_math_expr(*expr),
+
+            _ => unreachable!(),
         }
     }
 }
