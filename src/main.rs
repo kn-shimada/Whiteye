ca617d08f8fd6eff7b2dc0331514ad366742954b
kn-shimada <xianrendaotian012@gmail.com>
2021-08-12 22:56:00 +0900

    feat(main): Implemented logger function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
+use clap::{crate_description, crate_name, crate_version, App, Arg};
+use log::{debug, LevelFilter};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::process::exit;
-
-use clap::{crate_description, crate_name, crate_version, App, Arg};
 
 use whiteye::machine::Machine;
 use whiteye::parser::parse;
@@ -12,9 +12,22 @@ fn main() -> Result<()> {
     let app = App::new(crate_name!())
         .about(crate_description!())
         .version(crate_version!())
-        .arg(Arg::with_name("FILE"));
+        .arg(Arg::with_name("FILE"))
+        .arg(Arg::with_name("dbg").short("d").long("debug"));
 
     let matches = app.get_matches();
+
+    let is_debug = matches.occurrences_of("dbg") != 0;
+
+    let mut logger = env_logger::Builder::new();
+
+    if is_debug {
+        logger.filter_level(LevelFilter::Debug);
+    } else {
+        logger.filter_level(LevelFilter::Info);
+    }
+
+    logger.init();
 
     if let Some(path) = matches.value_of("FILE") {
         let f = File::open(path).unwrap();
@@ -24,17 +37,20 @@ fn main() -> Result<()> {
 
         for line in reader.lines() {
             let line = line.unwrap();
-            println!("Raw: {}", line);
+
+            debug!("Raw: {}", line);
 
             let (input, parsed) = parse(&line).unwrap();
             if !input.is_empty() {
                 eprintln!("parsing error, input remaining {:?}", input);
                 exit(1);
             }
-            println!("AST: {:?}", parsed);
+
+            debug!("AST: {:?}", parsed);
 
             machine.run(parsed);
-            println!("machine state: {:?}", machine);
+
+            debug!("machine state: {:?}", machine);
         }
     }
 
